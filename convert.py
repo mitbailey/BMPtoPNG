1bd81a0d4b1943943b834589869df64e1b925dd3
Mit Bailey <mitbailey@outlook.com>
2023-07-25 10:20:54 -0400

    Update.

--- convert.py
+++ convert.py
@@ -1,10 +1,25 @@
 from PIL import Image
-import glob
 import os
 
-outdir = '.'
-indir = '.'
-for filename in os.listdir(indir):
-    if filename.endswith('.bmp') or filename.endswith('.BMP'):
-        print('Converting %s to PNG.'%(filename))
-        Image.open(filename).save(os.path.join(outdir, '%s.png'%(filename)), format='png')
+indir = './input'
+outdir = './output'
+
+if not os.path.exists(indir):
+    print('The input directory cannot be found. Please ensure all PNG files to be converted are placed in a folder named "input" at the same level as the executable.')
+    exit(1)
+
+if not os.path.exists(outdir):
+    print('The output directory cannot be found. Please ensure there exists a folder named "output" at the same level as the executable.')
+    exit(2)
+
+print('Converting files:\n')
+
+infilelist = os.listdir(indir)
+infilelist = [name for name in infilelist if '.bmp' in name or '.BMP' in name]
+
+for infile in infilelist:
+    outfile = infile.rsplit('.', 1)[0]
+    print('%s --> %s.png'%(infile, outfile))
+    Image.open('%s/%s'%(indir, infile)).save(os.path.join(outdir, '%s.png'%(outfile)), format='png')
+
+print('\nComplete.')
